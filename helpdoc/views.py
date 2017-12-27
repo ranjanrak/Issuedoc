c2a919f19ca4d1d08b17574664f9fee97e0d1461
Rakesh <ranjan01995@gmail.com>
2017-12-27 19:03:18 +0530

    Added default end date as today

--- helpdoc/views.py
+++ helpdoc/views.py
@@ -105,12 +105,12 @@ def issue(request):
 		if tag == "All":
 			issue=Issue.objects.order_by('-date')
 			if date:
-				issue=Issue.objects.filter(date__range=[date,datetime.date.today])
+				issue=Issue.objects.filter(date__range=[date,datetime.date.today()])
 			if date and date1:
 				issue=Issue.objects.filter(date__range=[date,date1])		
 		else:
 			if date:	
-				issue=Issue.objects.filter(tag=tag,tag2=tag2,date__range=[date,datetime.date.today]).order_by('-date')
+				issue=Issue.objects.filter(tag=tag,tag2=tag2,date__range=[date,datetime.date.today()]).order_by('-date')
 			if date and date1:	
 				issue=Issue.objects.filter(tag=tag,tag2=tag2,date__range=[date,date1]).order_by('-date')	
 			else:
