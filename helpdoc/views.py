82133d7be6bf905a0811929d7fc44ddcae69169a
Rakesh <ranjan01995@gmail.com>
2017-12-29 16:04:57 +0530

    Test3

--- helpdoc/views.py
+++ helpdoc/views.py
@@ -104,23 +104,23 @@ def issue(request):
 		date=request.POST['date1']
 		date1=request.POST['date2']
 		#Too many conditions are coming as per feedback
-		if tag == "All":
+		if tag == "All" and tag2!= "All" and tag3 == "All":
+			issue=Issue.objects.order_by('-date')
 			if date:
 				issue=Issue.objects.filter(date__range=[date,datetime.date.today()]).order_by('date')
 			if date and date1:
 				issue=Issue.objects.filter(date__range=[date,date1]).order_by('date')
-			else:
-				issue=Issue.objects.order_by('-date')					
+	
+
+		if tag!="All" and tag2 == "All" and tag3 == "All":
+			issue=Issue.objects.filter(tag=tag).order_by('-date')
+			if date:	
+				issue=Issue.objects.filter(tag=tag,date__range=[date,datetime.date.today()]).order_by('date')
+			if date and date1:	
+				issue=Issue.objects.filter(tag=tag,date__range=[date,date1]).order_by('date')				
 
 		else:
-			if tag!="All" and tag2 == "All" and tag3 == "All":
-				issue=Issue.objects.filter(tag=tag).order_by('-date')
-				if date:	
-					issue=Issue.objects.filter(tag=tag,date__range=[date,datetime.date.today()]).order_by('date')
-				if date and date1:	
-					issue=Issue.objects.filter(tag=tag,date__range=[date,date1]).order_by('date')
-					
-			if tag!="All" and tag2!= "All" and tag3 == "All":
+			if tag2!= "All" and tag3 == "All":
 				issue=Issue.objects.filter(tag=tag,tag2=tag2).order_by('-date')		
 				if date:	
 					issue=Issue.objects.filter(tag=tag,tag2=tag2,date__range=[date,datetime.date.today()]).order_by('date')
