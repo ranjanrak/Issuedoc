e39ce4bcfb514ed2e317ace7bcaf1f48b907cc91
Rakesh <ranjan01995@gmail.com>
2017-12-29 18:22:01 +0530

    Improved algo

--- helpdoc/views.py
+++ helpdoc/views.py
@@ -104,14 +104,6 @@ def issue(request):
 		date=request.POST['date1']
 		date1=request.POST['date2']
 		#Too many conditions are coming as per feedback
-		if tag == "All" and tag2!= "All" and tag3 == "All":
-			issue=Issue.objects.order_by('-date')
-			if date:
-				issue=Issue.objects.filter(date__range=[date,datetime.date.today()]).order_by('date')
-			if date and date1:
-				issue=Issue.objects.filter(date__range=[date,date1]).order_by('date')
-	
-
 		if tag!="All" and tag2 == "All" and tag3 == "All":
 			issue=Issue.objects.filter(tag=tag).order_by('-date')
 			if date:	
