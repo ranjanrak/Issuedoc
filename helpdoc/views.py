3c67fa90beb4bfe95cd79f974fbae84bf3b6fb27
Rakesh <ranjan01995@gmail.com>
2017-12-29 14:12:04 +0530

    Condition Bug fix

--- helpdoc/views.py
+++ helpdoc/views.py
@@ -111,14 +111,14 @@ def issue(request):
 			if date and date1:
 				issue=Issue.objects.filter(date__range=[date,date1]).order_by('date')		
 		else:
-			if tag3 == "All":
+			if tag2 == "OverAll" and tag3 == "All":
 				issue=Issue.objects.filter(tag=tag).order_by('-date')
 				if date:	
 					issue=Issue.objects.filter(tag=tag,date__range=[date,datetime.date.today()]).order_by('date')
 				if date and date1:	
 					issue=Issue.objects.filter(tag=tag,date__range=[date,date1]).order_by('date')
 
-			if tag2!= "All" and tag3 == "All":
+			if tag2!= "OverAll" and tag3 == "All":
 				issue=Issue.objects.filter(tag=tag,tag2=tag2).order_by('-date')		
 				if date:	
 					issue=Issue.objects.filter(tag=tag,tag2=tag2,date__range=[date,datetime.date.today()]).order_by('date')
@@ -213,11 +213,11 @@ def report_category(request):
 def jsondata(request):
 	""" Sending the JSON data for Ajax call """
 	data={
-	'Q':'T1 Holdings/avg,T2 Holdings/avg,Position avg,Settlement Holiday,Others,All',
-	'kite':'Holdings,Login,Market watch,Orders,Positions,Charts,Holdings/Orders/Positions,Orders/Positions,All',
-	'Pi':'Holdings,Login,Market watch,Orders,Positions,Charts,All ',
-	'Nest':'Holdings,login,Orders,Positions,All',
-	'Main':'Holdings,Orders,Positions,All',
+	'Q':'T1 Holdings/avg,T2 Holdings/avg,Position avg,Settlement Holiday,Others,OverAll',
+	'kite':'Holdings,Login,Market watch,Orders,Positions,Charts,Holdings/Orders/Positions,Orders/Positions,OverAll',
+	'Pi':'Holdings,Login,Market watch,Orders,Positions,Charts,OverAll',
+	'Nest':'Holdings,login,Orders,Positions,OverAll',
+	'Main':'Holdings,Orders,Positions,OverAll',
 	'All':'All'
 	}
 	return JsonResponse(data)    				                     			
