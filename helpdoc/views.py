241a3b3565a5793ad3351bc5f8ae0a59b8252271
Rakesh <ranjan01995@gmail.com>
2018-01-03 19:13:51 +0530

    Adding more query condition as per feedback

--- helpdoc/views.py
+++ helpdoc/views.py
@@ -109,7 +109,14 @@ def issue(request):
 			if date:	
 				issue=Issue.objects.filter(tag=tag,date__range=[date,datetime.date.today()]).order_by('date')
 			if date and date1:	
-				issue=Issue.objects.filter(tag=tag,date__range=[date,date1]).order_by('date')				
+				issue=Issue.objects.filter(tag=tag,date__range=[date,date1]).order_by('date')
+
+		if tag!="All" and tag2 == "All" and tag3! = "All":
+			issue=Issue.objects.filter(tag=tag,tag3=tag3).order_by('-date')
+			if date:	
+				issue=Issue.objects.filter(tag=tag,tag3=tag3,date__range=[date,datetime.date.today()]).order_by('date')
+			if date and date1:	
+				issue=Issue.objects.filter(tag=tag,tag3=tag3,date__range=[date,date1]).order_by('date')						
 
 		else:
 			if tag!= "All" and tag2!= "All":
